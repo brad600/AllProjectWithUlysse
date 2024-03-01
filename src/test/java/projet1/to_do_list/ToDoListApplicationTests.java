1c2c77510b20cfe3506ab4e640ca50e756caaf92
brad <harolbradnjontu@gmail.com>
2024-03-01 21:57:22 +0100

    projet 1  ToDoList

---
+++ src/test/java/projet1/to_do_list/ToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package projet1.to_do_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
